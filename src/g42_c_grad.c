85671666cea43b153422dff2fc40580c3cb6667a
kafkalainen <joonas.hj.nivala@gmail.com>
2020-11-02 12:34:09 +0200

    Fixed gradient.

--- src/g42_c_grad.c
+++ src/g42_c_grad.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 16:59:02 by jnivala           #+#    #+#             */
-/*   Updated: 2020/11/02 09:35:58 by jnivala          ###   ########.fr       */
+/*   Updated: 2020/11/02 12:33:19 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static double	g42_traveled_dist(t_uv *p0, t_uv *p1, t_uv *s)
 	return (trav_dist / total_dist);
 }
 
-int				g42_horizontal_line(double scale, t_colour *brightest)
+static int		g42_gradient_line(double scale, t_colour *brightest)
 {
 	t_colour	new;
 
@@ -52,27 +52,23 @@ int				g42_horizontal_line(double scale, t_colour *brightest)
 int				g42_c_grad(t_uv p0, t_uv p1, t_uv s, int colour)
 {
 	t_colour	brightest;
-	t_colour	new;
 	double		frac;
+	double		current;
 
 	frac = g42_traveled_dist(&p0, &p1, &s);
 	brightest = g42_hex_to_trgb(colour);
 	if (p0.s_scale > p1.s_scale)
 	{
-		new.t = (int)((frac * p0.s_scale) * brightest.t);
-		new.r = (int)((frac * p0.s_scale) * brightest.r);
-		new.g = (int)((frac * p0.s_scale) * brightest.g);
-		new.b = (int)((frac * p0.s_scale) * brightest.b);
-		return (g42_trgb_to_hex(new));
+		current = frac * p0.s_scale + p1.s_scale;
+		current = current > 1.0 ? p0.s_scale : current;
+		return (g42_gradient_line(current, &brightest));
 	}
 	else if (p0.s_scale == p1.s_scale)
-		return (g42_horizontal_line(p0.s_scale, &brightest));
+		return (g42_gradient_line(p0.s_scale, &brightest));
 	else
 	{
-		new.t = (int)((1 - frac * p1.s_scale) * brightest.t);
-		new.r = (int)((1 - frac * p1.s_scale) * brightest.r);
-		new.g = (int)((1 - frac * p1.s_scale) * brightest.g);
-		new.b = (int)((1 - frac * p1.s_scale) * brightest.b);
-		return (g42_trgb_to_hex(new));
+		current = p1.s_scale - frac * p1.s_scale + p0.s_scale;
+		current = current > p1.s_scale ? p1.s_scale : current;
+		return (g42_gradient_line(current, &brightest));
 	}
 }
